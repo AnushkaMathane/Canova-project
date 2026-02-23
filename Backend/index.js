e8db613b2c80b4e54ffcc979e64f04c8fecef021
Anushka Mathane <anumathane1@gmail.com>
2026-02-23 21:46:33 +0530

    fix backend start file and render deploy

--- Backend/index.js
+++ Backend/index.js
@@ -4,28 +4,31 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
-const userRoute = require('./src/routes/userRoute')
-const projectsRoute = require('./src/routes/projectRoute')
-const formsRoute = require('./src/routes/formsRoute')
-const pagesRoute = require('./src/routes/pagesRoute')
-const draftPublishRoute = require('./src/routes/draftPublishRoute')
-const publicRoute = require('./src/routes/publicRoute')
+const userRoute = require("./src/routes/userRoute");
+const projectsRoute = require("./src/routes/projectRoute");
+const formsRoute = require("./src/routes/formsRoute");
+const pagesRoute = require("./src/routes/pagesRoute");
+const draftPublishRoute = require("./src/routes/draftPublishRoute");
+const publicRoute = require("./src/routes/publicRoute");
 const { connectDB } = require("./src/config/ConnectDB");
-
 
 app.use(cors());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-connectDB()
+connectDB();
 
-app.use('/api/auth', userRoute)
-app.use('/api/projects', projectsRoute)
-app.use('/api/forms', formsRoute)
-app.use('/api/pages', pagesRoute)
-app.use('/api/draftPublish', draftPublishRoute)
-app.use('/api/public', publicRoute)
+app.use("/api/auth", userRoute);
+app.use("/api/projects", projectsRoute);
+app.use("/api/forms", formsRoute);
+app.use("/api/pages", pagesRoute);
+app.use("/api/draftPublish", draftPublishRoute);
+app.use("/api/public", publicRoute);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 app.listen(port, () => {
   console.log(`Server is runnning at Port: ${port}`);
